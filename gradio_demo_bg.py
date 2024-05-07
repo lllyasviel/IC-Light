7173fc3d8a43fd075cad29d440f43486bda79eb3
layerdiffusion <19834515+lllyasviel@users.noreply.github.com>
2024-05-07 13:05:53 -0700

    Update gradio_demo_bg.py

--- gradio_demo_bg.py
+++ gradio_demo_bg.py
@@ -434,25 +434,32 @@ with block:
                 input_fg = gr.Image(source='upload', type="numpy", label="Foreground", height=480)
                 input_bg = gr.Image(source='upload', type="numpy", label="Background", height=480)
             prompt = gr.Textbox(label="Prompt")
+            bg_source = gr.Radio(choices=[e.value for e in BGSource],
+                                 value=BGSource.UPLOAD.value,
+                                 label="Background Source", type='value')
+
             example_inpaint_prompts = gr.Dataset(samples=quick_prompts,
                                                  label='Prompt Quick List',
                                                  components=[prompt])
             relight_button = gr.Button(value="Relight")
             normal_button = gr.Button(value="Compute Normal (4x Slower)")
-            bg_source = gr.Radio(choices=[e.value for e in BGSource],
-                                 value=BGSource.UPLOAD.value,
-                                 label="Background Source", type='value')
-            num_samples = gr.Slider(label="Images", minimum=1, maximum=12, value=1, step=1)
-            seed = gr.Slider(label="Seed", minimum=-1, maximum=2147483647, step=1, value=12345)
-            image_width = gr.Slider(label="Image Width", minimum=256, maximum=1024, value=512, step=64)
-            image_height = gr.Slider(label="Image Height", minimum=256, maximum=1024, value=640, step=64)
-            highres_scale = gr.Slider(label="Highres Scale", minimum=1.0, maximum=3.0, value=1.5, step=0.01)
+
+            with gr.Group():
+                with gr.Row():
+                    num_samples = gr.Slider(label="Images", minimum=1, maximum=12, value=1, step=1)
+                    seed = gr.Number(label="Seed", value=12345, precision=0)
+                with gr.Row():
+                    image_width = gr.Slider(label="Image Width", minimum=256, maximum=1024, value=512, step=64)
+                    image_height = gr.Slider(label="Image Height", minimum=256, maximum=1024, value=640, step=64)
+
             with gr.Accordion("Advanced options", open=False):
                 steps = gr.Slider(label="Steps", minimum=1, maximum=100, value=20, step=1)
                 cfg = gr.Slider(label="CFG Scale", minimum=1.0, maximum=32.0, value=7.0, step=0.01)
+                highres_scale = gr.Slider(label="Highres Scale", minimum=1.0, maximum=3.0, value=1.5, step=0.01)
                 highres_denoise = gr.Slider(label="Highres Denoise", minimum=0.1, maximum=0.9, value=0.5, step=0.01)
-                a_prompt = gr.Textbox(label="Added Prompt", value='best quality, extremely detailed, 4k, 8k, 35mm')
-                n_prompt = gr.Textbox(label="Negative Prompt", value='lowres, bad anatomy, bad hands, cropped, worst quality')
+                a_prompt = gr.Textbox(label="Added Prompt", value='best quality')
+                n_prompt = gr.Textbox(label="Negative Prompt",
+                                      value='lowres, bad anatomy, bad hands, cropped, worst quality')
         with gr.Column():
             result_gallery = gr.Gallery(height=768, object_fit='contain', label='Outputs')
     ips = [input_fg, input_bg, prompt, image_width, image_height, num_samples, seed, steps, a_prompt, n_prompt, cfg, highres_scale, highres_denoise, bg_source]
